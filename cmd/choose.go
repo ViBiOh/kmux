e03b098f0e69218c2427960e3cb66234c1e4c834
Vincent Boutour <bob@vibioh.fr>
2022-08-04 14:13:26 +0200

    fix: missing file
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

---
+++ cmd/choose.go
@@ -0,0 +1,65 @@
+package cmd
+
+import (
+	"context"
+	"fmt"
+	"sort"
+
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+)
+
+//nolint deadcode
+func getAllDeployments(name string) []string {
+	output := make(chan string, len(clients))
+	successChan := make(chan bool, len(clients))
+
+	go func() {
+		defer close(output)
+		defer close(successChan)
+
+		clients.execute(func(contextName string, client kubeClient) error {
+			list, err := client.clientset.AppsV1().Deployments("").List(context.Background(), v1.ListOptions{})
+			if err != nil {
+				return err
+			}
+
+			for _, deployment := range list.Items {
+				output <- fmt.Sprintf("%s/%s", deployment.GetNamespace(), deployment.GetName())
+			}
+
+			successChan <- true
+
+			return nil
+		})
+	}()
+
+	var items []string
+	for item := range output {
+		items = append(items, item)
+	}
+	sort.Strings(items)
+
+	var successCount uint64
+	for range successChan {
+		successCount += 1
+	}
+
+	deduplicated := items[:0]
+	var count uint64
+	var previous string
+
+	for _, item := range items {
+		if item == previous {
+			count += 1
+		} else {
+			count = 1
+			previous = item
+		}
+
+		if count == successCount {
+			deduplicated = append(deduplicated, previous)
+		}
+	}
+
+	return deduplicated
+}
