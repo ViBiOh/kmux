afcab4b40810eea4e1a51577464f7fbb431934ba
Vincent Boutour <bob@vibioh.fr>
2022-10-08 22:51:18 +0200

    refactor: Simplifying sha computation
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- pkg/sha/sha.go
+++ pkg/sha/sha.go
@@ -4,20 +4,15 @@ import (
 	"crypto/sha256"
 	"encoding/hex"
 	"encoding/json"
-	"fmt"
 )
-
-// New get sha256 value of given interface.
-func New(content any) string {
-	hasher := sha256.New()
-
-	// no err check https://golang.org/pkg/hash/#Hash
-	_, _ = fmt.Fprintf(hasher, "%#v", content)
-
-	return hex.EncodeToString(hasher.Sum(nil))
-}
 
 func JSON(content any) string {
 	value, _ := json.Marshal(content)
-	return New(value)
+
+	hasher := sha256.New()
+
+	// no err check https://golang.org/pkg/hash/#Hash
+	_, _ = hasher.Write(value)
+
+	return hex.EncodeToString(hasher.Sum(nil))
 }
--- pkg/sha/sha_test.go
+++ pkg/sha/sha_test.go
@@ -2,32 +2,6 @@ package sha
 
 import "testing"
 
-func TestNew(t *testing.T) {
-	type args struct {
-		content any
-	}
-
-	cases := map[string]struct {
-		args args
-		want string
-	}{
-		"simple": {
-			args{
-				content: "test",
-			},
-			"4d967a30111bf29f0eba01c448b375c1629b2fed01cdfcc3aed91f1b57d5dd5e",
-		},
-	}
-
-	for intention, tc := range cases {
-		t.Run(intention, func(t *testing.T) {
-			if got := New(tc.args.content); got != tc.want {
-				t.Errorf("New() = `%s`, want `%s`", got, tc.want)
-			}
-		})
-	}
-}
-
 func TestJSON(t *testing.T) {
 	t.Parallel()
 
@@ -49,7 +23,7 @@ func TestJSON(t *testing.T) {
 					ID: 8000,
 				},
 			},
-			"4a0946ec5089c35bfcf65b5e03dce607a5908fe4dda87dfa71744354b5805779",
+			"b2a15a2509fce509931bc2204fd197def8adb7223ee697d746c71c7ce128b325",
 		},
 	}
 
@@ -67,7 +41,7 @@ func TestJSON(t *testing.T) {
 	}
 }
 
-func BenchmarkNew(b *testing.B) {
+func BenchmarkJSON(b *testing.B) {
 	type testStruct struct {
 		ID int
 	}
@@ -75,6 +49,6 @@ func BenchmarkNew(b *testing.B) {
 	item := testStruct{}
 
 	for i := 0; i < b.N; i++ {
-		New(item)
+		JSON(item)
 	}
 }
