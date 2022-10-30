91a4fe8b08cecc202f539af637aeddae09b8e527
Vincent Boutour <bob@vibioh.fr>
2022-10-30 11:34:42 +0100

    fix(restart): Wrong expected argument number
    Signed-off-by: Vincent Boutour <bob@vibioh.fr>

--- cmd/restart.go
+++ cmd/restart.go
@@ -55,7 +55,7 @@ var restartCmd = &cobra.Command{
 
 		return nil, cobra.ShellCompDirectiveNoFileComp
 	},
-	Args: cobra.MatchAll(cobra.ExactArgs(3), cobra.OnlyValidArgs),
+	Args: cobra.MatchAll(cobra.ExactArgs(2), cobra.OnlyValidArgs),
 	Run: func(cmd *cobra.Command, args []string) {
 		resourceType := args[0]
 		resourceName := args[1]
